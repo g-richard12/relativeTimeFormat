d81a44f553ac7bbb226cbcd6b29afca0a16fe36c
richard <romp3206@gmail.com>
2024-02-15 16:54:56 +0800

    change name

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import relative_time_format from "./relativeTimeFormat.js";
-relative_time_format("en-US")(+new Date() - 60000, (time) => {
+import transform_time_unit from "./transformTimeUnit.js";
+transform_time_unit("en-US")(+new Date() - 60000, (time) => {
   console.log(time);
 });
